53a476b1c2ed45dbb54076757735eee7d2ce0af0
OGStefs <stefanistweg@gmail.com>
2023-05-27 19:46:41 +0200

    added styling

--- resources/web3.js
+++ resources/web3.js
@@ -19,7 +19,8 @@ async function connect(e) {
           btn.removeEventListener("click", (e) => connect(e));
         })
         .then(() => getOwned(walletAddress))
-        .then(() => getForged(walletAddress));
+        .then(() => getForged(walletAddress))
+        .then(() => (window.location = "#venture"));
     } catch (error) {
       console.log(error);
     }
@@ -97,7 +98,7 @@ async function getForged(walletAddress) {
   let a = web3.utils.toChecksumAddress(walletAddress);
   //   console.log("converted", a);
   //   console.log("forgies", x[a]);
-  //   console.log("the X", x);
+  console.log("the X", x);
   if (x) allforged = x[a]?.forged;
 
   console.log("Forged: " + String(allforged));
